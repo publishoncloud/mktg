321afffc77910247b94a39db70282a6a6e9d5699
publishoncloud <40393354+publishoncloud@users.noreply.github.com>
2018-06-19 18:58:21 +0530

    Update pubgen.js

--- pubgen.js
+++ pubgen.js
@@ -9,5 +9,5 @@ ad_box.style.backgroundSize='cover';
 ad_box.style.backgroundPosition='center center';
 ad_box.style.position='relative';
 ad_box.style.overflow='hidden';
-var ad_data = '<div class="ad_text" style="position: absolute;width: 300px;height: 60px;background: rgba(0, 0, 0, 0.70);color: #FFF; border-radius: 0% 0% 100px 0%;line-height: 60px;font-weight: bold;font-size: 20px;"><span style="font-size: 30px;padding-right: 5px;padding-left: 40px;">Book</span><span>xy</span></div><a href="http://pubgen.com/" target="_blank" sty><div style="width:100%;height:100%;top:0px;left:0px;"></div></a>';
+var ad_data = '<div class="ad_text" style="position: absolute;width: 300px;height: 60px;background: rgba(0, 0, 0, 0.70);color: #FFF; border-radius: 0% 0% 100px 0%;line-height: 60px;font-weight: bold;font-size: 20px;"><span style="font-size: 30px;padding-right: 5px;padding-left: 40px;">Book</span><span>xyz</span></div><a href="http://pubgen.com/" target="_blank" sty><div style="width:100%;height:100%;top:0px;left:0px;"></div></a>';
 ad_box.innerHTML=ad_data;
